68f621c9c909dff63dea186acd80d1109417c356
Hyunseo Kim <alzflost@naver.com>
2023-05-21 17:25:27 +0900

    header added

--- web/main.dart.js
+++ web/main.dart.js
@@ -71905,7 +71905,7 @@ ZG(a){var s=0,r=A.a0(t.z),q=this,p,o,n,m
 var $async$oY=A.a1(function(b,c){if(b===1)return A.Y(c,r)
 while(true)switch(s){case 0:q.e.oz(0,B.ID)
 q.aw(new A.a7q(q,new A.tA(a,!0,new A.dN(Date.now(),!1))))
-p=A.nY("127.0.0.1:8000/chat/")
+p=A.nY("http://127.0.0.1:8000/chat/")
 o=t.N
 n=A.av(["X-CSRFToken","rqypHUi5IvhXalmYNbZnb6jBRK33fVQt"],o,o)
 s=2
